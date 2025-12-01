dbe04e61b670adfa97c75f86092de2d0d4fb274e
DENYS <132446947+hyppolit@users.noreply.github.com>
2025-12-01 12:03:28 +0100

    Format de déconnexion.php pour la clarté

--- gymflow/deconnexion.php
+++ gymflow/deconnexion.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
 
-// Détruire toutes les sessions
+
 session_unset();
 session_destroy();
 
-// Redirection vers la page d'accueil
+
 header("Location: main.html");
 exit;
 ?>
+
